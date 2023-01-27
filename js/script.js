18fff0fccc247f05200265befe20433174358aa2
silva <thediamondheart890@gmail.com>
2023-01-28 01:05:45 +0400

    add a color picker that changes the color,rainbow mode

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ container.classList.add(`container`);
 const submit = document.querySelector('.submit-grid');
 const inputOfGrid = document.querySelector('.divs-num');
 
+const inputOfColor = document.querySelector('#color-picker')
+
+const rainbowMode = document.querySelector('.rainbow')
+
 let num;
 function divsNum (numForDivs = 16){
 
@@ -27,6 +31,8 @@ console.log(num);
 }
 
 
+
+
 divsNum();
 sketch();
 
@@ -41,14 +47,19 @@ else if (numOfDivsEachSide >= 1 && numOfDivsEachSide <= 64){
     divsNum(numOfDivsEachSide);
 }
 else {
-    alert("no no")
+    alert("Please Type a number that is between 1 and 64")
     return;
 }
 sketch();
 })
 
 let mouseOver = function() {
-    this.classList.add('square-down');
+    this.setAttribute('style', `background-color: ${getColor()};`);
+}
+
+
+let mouseOverRainbow = function() {
+    this.setAttribute('style', `background-color: ${getRainbowColor()};`);
 }
 
 
@@ -57,16 +68,63 @@ function sketch (){
 let squares = document.querySelectorAll('.square');
 squares.forEach((square) => {
 square.addEventListener('click', () => {
-square.classList.add('square-down');
+    square.setAttribute('style', `background-color: ${getColor()};`);
 })
 window.addEventListener('mouseup', () => {
-square.removeEventListener('mouseenter', mouseOver);
+    square.removeEventListener('mouseenter', mouseOver);
 })
 window.addEventListener('mousedown', () => {
-square.addEventListener('mouseenter', mouseOver);
+    square.addEventListener('mouseenter', mouseOver);
 })
 
 
 })
 }
 
+let flag = true;
+
+function getColor(){
+  if (flag) {
+    let color = inputOfColor.value;
+    return color;
+  }
+  if (flag === false){
+    return getRainbowColor();
+  }
+}
+
+function getRainbowColor(){
+    let randomNum = Math.random();
+    if (randomNum > 0 && randomNum <= 0.2){
+        return '#34ebe8';   
+    }
+    else if (randomNum >= 0.2 && randomNum <= 0.4){
+        return '#f556d2';
+
+    }
+    else if (randomNum >= 0.4 && randomNum <= 0.5){
+        return '#9000ff';
+
+    }
+    else if (randomNum >= 0.5 && randomNum <= 0.6){
+        return '#918d90';
+    }
+    else if (randomNum >= 0.6 && randomNum <= 0.7){
+        return '#40de28';
+    }
+    else if (randomNum >= 0.7 && randomNum <= 0.8){
+        return '#ff0303';
+    }
+    else if (randomNum >=0.8 && randomNum <= 0.9){
+        return '#eeff00';
+    }
+    else if (randomNum >= 0.9 && randomNum <= 1){
+        return '#0703ff';
+    }
+
+}
+
+
+rainbowMode.addEventListener('click', () => {
+    flag = !flag;
+})
