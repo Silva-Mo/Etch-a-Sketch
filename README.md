bec5b947b4ee13e920a3569ca785786b0641f6a1
silva <thediamondheart890@gmail.com>
2023-01-29 20:47:56 +0400

    add simple GUI and other interactive functions add the following functions: -erase -clear -change grid background color depending on which color the user will pick -add lines

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Etch-a-Sketch
+<!-- colors scheme url :https://coolors.co/353535-ffffff-d2d7df-bdbbb0-8a897c -->
--- js/script.js
+++ js/script.js
@@ -3,29 +3,35 @@ container.classList.add(`container`);
 
 const submit = document.querySelector('.submit-grid');
 const inputOfGrid = document.querySelector('.divs-num');
-const inputOfColor = document.querySelector('#color-picker')
-const rainbowMode = document.querySelector('.rainbow')
+const inputOfColor = document.querySelector('#color-picker-sketch');
+const rgb = document.querySelector('.rgb');
+const clearBtn = document.querySelector('.clear');
+const eraser = document.querySelector('.eraser');
+const backgroundColor = document.querySelector('#color-picker-grid');
+const submitGridColor = document.querySelector('.submit-color-grid');
+const gridLines = document.querySelector('.grid-lines');
 
 let num;
-function divsNum (numForDivs = 16){
+function divsNum(numForDivs = 16) {
 
-if (num !== undefined || num !== null || num !== ""){
-for (let i = num; i >= 1; i--){
-    let square = document.querySelector('.square');
-    container.removeChild(square);
-}}
+    if (num !== undefined || num !== null || num !== "") {
+        for (let i = num; i >= 1; i--) {
+            let square = document.querySelector('.square');
+            container.removeChild(square);
+        }
+    }
 
-container.setAttribute('style', `grid-template-columns: repeat(${numForDivs}, auto);
-grid-template-rows: repeat(${numForDivs}, auto);`)
-num = numForDivs * numForDivs;
-console.log(num);
-for (let i = 1; i < (num +1); i++) {
-    const square = document.createElement('div');
-    square.classList.add('square')
-    square.setAttribute('draggable', 'false');
-    container.appendChild(square);
-}
-console.log(num);
+    container.setAttribute('style', `grid-template-columns: repeat(${numForDivs}, auto);
+grid-template-rows: repeat(${numForDivs}, auto);`);
+    num = numForDivs * numForDivs;
+    console.log(num);
+    for (let i = 1; i < (num + 1); i++) {
+        const square = document.createElement('div');
+        square.classList.add('square')
+        square.setAttribute('draggable', 'false');
+        container.appendChild(square);
+    }
+    console.log(num);
 }
 
 
@@ -35,98 +41,152 @@ divsNum();
 sketch();
 
 submit.addEventListener('click', () => {
-let numOfDivsEachSide;
-numOfDivsEachSide = inputOfGrid.value;
-if(numOfDivsEachSide > 64) {
+    let numOfDivsEachSide;
+    numOfDivsEachSide = inputOfGrid.value;
+    if (numOfDivsEachSide > 64) {
         alert("Maximum is 64 !!")
         return;
-}
-else if (numOfDivsEachSide >= 1 && numOfDivsEachSide <= 64){
-    divsNum(numOfDivsEachSide);
-}
-else {
-    alert("Please Type a number that is between 1 and 64")
-    return;
-}
-sketch();
+    }
+    else if (numOfDivsEachSide >= 1 && numOfDivsEachSide <= 64) {
+        divsNum(numOfDivsEachSide);
+    }
+    else {
+        alert("Please Type a number that is between 1 and 64");
+        return;
+    }
+    sketch();
 })
 
-let mouseOver = function() {
+let mouseOver = function () {
     this.setAttribute('style', `background-color: ${getColor()};`);
 }
 
+function sketch() {
+    let squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener('click', () => {
+            square.setAttribute('style', `background-color: ${getColor()};`);
+        })
+        window.addEventListener('mouseup', () => {
+            square.removeEventListener('mouseenter', mouseOver);
+        })
+        window.addEventListener('mousedown', () => {
+            square.addEventListener('mouseenter', mouseOver);
+        })
 
-let mouseOverRainbow = function() {
-    this.setAttribute('style', `background-color: ${getRainbowColor()};`);
+
+
+    })
 }
 
-
-
-function sketch (){
-let squares = document.querySelectorAll('.square');
-squares.forEach((square) => {
-square.addEventListener('click', () => {
-    square.setAttribute('style', `background-color: ${getColor()};`);
-})
-window.addEventListener('mouseup', () => {
-    square.removeEventListener('mouseenter', mouseOver);
-})
-window.addEventListener('mousedown', () => {
-    square.addEventListener('mouseenter', mouseOver);
-})
-
-
-})
-}
-
+let flag3 = true;
+let flag2 = "";
 let flag = true;
 
-function getColor(){
-  if (flag) {
-    let color = inputOfColor.value;
-    return color;
-  }
-  if (flag === false){
-    return getRainbowColor();
-  }
+function getColor() {
+    if (flag2 === "") {
+        let color = inputOfColor.value;
+        return color;
+    }
+    else if (flag2 === "A") {
+        return getRGBColor();
+    }
 }
 
-function getRainbowColor(){
+function getRGBColor() {
     let randomNum = Math.random();
-    if (randomNum > 0 && randomNum <= 0.2){
-        return '#34ebe8';   
+    if (randomNum > 0 && randomNum <= 0.2) {
+        return '#34ebe8';
     }
-    else if (randomNum >= 0.2 && randomNum <= 0.4){
+    else if (randomNum >= 0.2 && randomNum <= 0.4) {
         return '#f556d2';
 
     }
-    else if (randomNum >= 0.4 && randomNum <= 0.5){
+    else if (randomNum >= 0.4 && randomNum <= 0.5) {
         return '#9000ff';
 
     }
-    else if (randomNum >= 0.5 && randomNum <= 0.6){
+    else if (randomNum >= 0.5 && randomNum <= 0.6) {
         return '#918d90';
     }
-    else if (randomNum >= 0.6 && randomNum <= 0.7){
+    else if (randomNum >= 0.6 && randomNum <= 0.7) {
         return '#40de28';
     }
-    else if (randomNum >= 0.7 && randomNum <= 0.8){
+    else if (randomNum >= 0.7 && randomNum <= 0.8) {
         return '#ff0303';
     }
-    else if (randomNum >=0.8 && randomNum <= 0.9){
+    else if (randomNum >= 0.8 && randomNum <= 0.9) {
         return '#eeff00';
     }
-    else if (randomNum >= 0.9 && randomNum <= 1){
+    else if (randomNum >= 0.9 && randomNum <= 1) {
         return '#0703ff';
     }
 
 }
 
 
-rainbowMode.addEventListener('click', () => {
+rgb.addEventListener('click', () => {
     flag = !flag;
+    if (flag === false) {
+        flag2 = "A";
+    }
+    else if (flag) {
+        flag2 = "";
+    }
 })
 
 inputOfColor.addEventListener('click', () => {
-    flag = true;
+    flag2 = "";
+})
+
+function clear() {
+    let squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.removeAttribute('style');
+    })
+}
+
+clearBtn.addEventListener('click', clear);
+
+function erase() {
+    this.removeAttribute('style');
+}
+
+eraser.addEventListener('click', () => {
+    flag3 = !flag3;
+    if (flag3 === false) {
+        flag2 = "B";
+    }
+    else if (flag3) {
+        if (flag === false) {
+            flag2 = "A";
+        }
+        else if (flag) {
+            flag2 = "";
+        }
+
+    }
+})
+
+
+
+submitGridColor.addEventListener('click', () => {
+  
+    let squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        let gridColor = backgroundColor.value;
+        document.getElementsByTagName('style')[0].innerHTML =`.square-changed {background-color:${gridColor};}`;
+        square.classList.add('square-changed');
+    })
+})
+
+let flag4 = true;
+gridLines.addEventListener('click', () => {
+flag4 = !flag4;
+if (flag4){
+    container.classList.remove('container-lined');
+}
+else if (flag4 === false) {
+    container.classList.add('container-lined');
+}
 })
