4913602237c3bee6c4e8b0b75c0a233866c282b5
silva <thediamondheart890@gmail.com>
2023-02-20 00:01:23 +0400

    add grabber, darken, lighten features

--- js/script.js
+++ js/script.js
@@ -10,7 +10,13 @@ const eraser = document.querySelector('.eraser');
 const backgroundColor = document.querySelector('#color-picker-grid');
 const submitGridColor = document.querySelector('.submit-color-grid');
 const gridLines = document.querySelector('.grid-lines');
+const grabberBtn = document.querySelector('.grabber');
+const darkenBtn = document.querySelector('.darken');
+const lightenBtn = document.querySelector('.lighten');
 
+let lightenFlag = false;
+let darkenFlag = false;
+let grabberFlag = false;
 let num;
 function divsNum(numForDivs = 16) {
 
@@ -25,7 +31,6 @@ function divsNum(numForDivs = 16) {
     container.setAttribute('style', `grid-template-columns: repeat(${numForDivs}, auto);
 grid-template-rows: repeat(${numForDivs}, auto);`);
     num = numForDivs * numForDivs;
-    console.log(num);
     for (let i = 1; i < (num + 1); i++) {
         const square = document.createElement('div');
         square.classList.add('square')
@@ -60,32 +65,123 @@ let mouseOver = function (e) {
     if (square === null){
         return;
     }
-    if (flag2){
-    square.setAttribute('style', `background-color: ${getColor()};`);
+    if (lightenFlag === false){
+        if (darkenFlag === false){
+            if (flag2){
+            square.setAttribute('style', `background-color: ${getColor()};`);
+            }
+            else if (flag2 === false){
+            square.removeAttribute('style');
+            }    
+        }
+        else if (darkenFlag){
+            if (flag2){
+                let currentColor = getCssSquareColor(square);
+                square.removeAttribute('style');
+                square.setAttribute('style', `background-color: ${darkenMode(currentColor)};`);
+            }
+                else if (flag2 === false){
+                square.removeAttribute('style');
+            }  
+        }    
     }
-    else if (flag2 === false){
-    square.removeAttribute('style');
+    else if (lightenFlag){
+        if (flag2){
+            let currentColor = getCssSquareColor(square);
+            square.removeAttribute('style');
+            square.setAttribute('style', `background-color: ${lightenMode(currentColor)};`);
+        }
+            else if (flag2 === false){
+            square.removeAttribute('style');
+        } 
     }
+    
   }
 }
+
+function lightenMode (rgbColor){
+        let sliced = rgbColor.slice(4);
+        let slicedEnd = sliced.slice(0, -1);
+        let array = slicedEnd.split(", ")
+        let red = Math.floor(+array[0] + (255 - array[0]) * 0.1);
+        let green = Math.floor(+array[1] + (255 - array[1]) * 0.1);  
+        let blue = Math.floor(+array[2] + (255 - array[2]) * 0.1);
+        if (red >= '246' && green >= '246' && blue >= '246'){
+            return '#ffffff'
+        }
+        let colorInHex = rgbToHex(+red, +green ,+blue);
+        return colorInHex;
+}
+
+function darkenMode (rgbColor) {
+        let sliced = rgbColor.slice(4);
+        let slicedEnd = sliced.slice(0, -1);
+        let array = slicedEnd.split(", ")
+        
+        let red = Math.round(array[0] * (1 - 0.2));
+        let green = Math.round(array[1] * (1 - 0.2));  
+        let blue = Math.round(array[2] * (1 - 0.2));
+        let colorInHex = rgbToHex(+red, +green ,+blue);
+        return colorInHex;
+}
+
 
 window.addEventListener('mousedown', () =>
   isDrawing = true)
 window.addEventListener('mouseup', () =>
   isDrawing = false);
 
-container.addEventListener('click', (e) => {
+function clickSquare (e) {
     let square = e.target.closest('.square');
     if (square === null){
         return;
     }
-    if (flag2){
-       square.setAttribute('style', `background-color: ${getColor()};`);  
+    if (grabberFlag === false){
+        if (lightenFlag === false){
+            if (darkenFlag === false){
+                if (flag2){
+                square.setAttribute('style', `background-color: ${getColor()};`);
+                }
+                else if (flag2 === false){
+                square.removeAttribute('style');
+                }    
+            }
+            else if (darkenFlag){
+                if (flag2){
+                    let currentColor = getCssSquareColor(square);
+                    square.removeAttribute('style');
+                    square.setAttribute('style', `background-color: ${darkenMode(currentColor)};`);
+                }
+                    else if (flag2 === false){
+                    square.removeAttribute('style');
+                }  
+            }    
+        }
+        else if (lightenFlag){
+            if (flag2){
+                let currentColor = getCssSquareColor(square);
+                square.removeAttribute('style');
+                square.setAttribute('style', `background-color: ${lightenMode(currentColor)};`);
+            }
+                else if (flag2 === false){
+                square.removeAttribute('style');
+            } 
+        }    
     }
-    else if (flag2 === false){
-        square.removeAttribute('style');
+    else if (grabberFlag === true){
+        let currentColor = getCssSquareColor(square);
+        let sliced = currentColor.slice(4);
+        let slicedEnd = sliced.slice(0, -1);
+        let array = slicedEnd.split(", ")
+        let red = array[0];
+        let green =  array [1];  
+        let blue = array [2]; 
+        let colorInHex = rgbToHex(+red, +green ,+blue);
+        inputOfColor.value = colorInHex;
     }
-})
+}
+
+container.addEventListener('click', clickSquare);
 container.addEventListener('mouseover', mouseOver);
 
 
@@ -114,13 +210,13 @@ function getRGBColor() {
         return '#ff66ff';
     }
     else if (randomNum >= 0.3 && randomNum <= 0.4) {
-        return '#000000';
+        return '#6C7A89';
     }
     else if (randomNum >= 0.4 && randomNum <= 0.5) {
         return '#00ffff';
     }
     else if (randomNum >= 0.5 && randomNum <= 0.6) {
-        return '#918d90';
+        return '#8D608C';
     }
     else if (randomNum >= 0.6 && randomNum <= 0.7) {
         return '#40de28';
@@ -141,6 +237,15 @@ function getRGBColor() {
 rgb.addEventListener('click', () => {
     flag = !flag;
     if (flag === false) {
+        lightenFlag = false;
+        darkenFlag = false;
+        lightenBtn.removeAttribute('style');
+        lightenBtn.textContent = 'Lighten';
+        darkenBtn.removeAttribute('style');
+        darkenBtn.textContent = 'Darken';
+        grabberFlag = false;
+        grabberBtn.textContent = 'Grabber';
+        grabberBtn.removeAttribute('style');
         flag2 = true;
         eraser.textContent ='Erase';
         eraser.removeAttribute('style');
@@ -154,20 +259,29 @@ rgb.addEventListener('click', () => {
         -o-animation: rainbow 4.5s ease infinite;
           animation: rainbow 4.5s ease infinite;
        `);
-        rgb.textContent = "RGB IS ON"
+        rgb.textContent = "RGB IS ON";
     }
     else if (flag) {
         rgb.removeAttribute('style');
-        rgb.textContent = 'TURN RGB ON'
+        rgb.textContent = 'RGB';
     }
 })
 
 inputOfColor.addEventListener('click', () => {
+    lightenFlag = false;
+    darkenFlag = false;
+    lightenBtn.removeAttribute('style');
+    lightenBtn.textContent = 'Lighten';
+    darkenBtn.removeAttribute('style');
+    darkenBtn.textContent = 'Darken';
+    grabberFlag = false;
+    grabberBtn.textContent = 'Grabber';
+    grabberBtn.removeAttribute('style');
     flag2 = true;
     flag = true;
     rgb.removeAttribute('style');
-    rgb.textContent = 'TURN RGB ON';
-    eraser.textContent ='Erase';
+    rgb.textContent = 'RGB';
+    eraser.textContent = 'Erase';
     eraser.removeAttribute('style');
     
 })
@@ -226,6 +340,110 @@ else if (flag3 === false) {
 }
 })
 
+function getCssSquareColor(element) {
+    let css_data = [];
+    let css_obj = getComputedStyle(element);
+  
+    for (let i = 0; i < css_obj.length; i++) {
+        css_data.push(`${css_obj.getPropertyValue(css_obj[i])}`)
+            
+    }
+    return css_data[20];
+}
+
+const rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => {
+    const hex = x.toString(16)
+    return hex.length === 1 ? '0' + hex : hex
+  }).join('')
+
+
+grabberBtn.addEventListener('click', () => {
+    grabberFlag = !grabberFlag
+    if (grabberFlag){
+        lightenFlag = false;
+        darkenFlag = false;
+        lightenBtn.removeAttribute('style');
+        lightenBtn.textContent = 'Lighten';
+        darkenBtn.removeAttribute('style');
+        darkenBtn.textContent = 'Darken';
+        flag2 = true;
+        flag = true;
+        rgb.removeAttribute('style');
+        rgb.textContent = 'RGB';
+        eraser.textContent = 'Erase';
+        eraser.removeAttribute('style');
+        grabberBtn.textContent = 'Grabber is ON';
+        grabberBtn.setAttribute('style', `
+        background-image: none;
+        background-color: white; 
+        color: #000000; 
+        border: 2px solid black; 
+        font-size: 15px;`);
+    }
+    else{
+        grabberBtn.textContent = 'Grabber';
+        grabberBtn.removeAttribute('style');
+    }
+});
+
+darkenBtn.addEventListener('click', () => {
+    darkenFlag = !darkenFlag;
+    if (darkenFlag){
+        flag2 = true;
+        flag = true;
+        rgb.removeAttribute('style');
+        rgb.textContent = 'RGB';
+        eraser.textContent = 'Erase';
+        eraser.removeAttribute('style');
+        grabberFlag = false;
+        grabberBtn.textContent = 'Grabber';
+        grabberBtn.removeAttribute('style');
+        lightenFlag = false;
+        lightenBtn.removeAttribute('style');
+        lightenBtn.textContent = 'Lighten';
+        darkenBtn.textContent = 'Darken is ON';
+        darkenBtn.setAttribute('style', `
+        background-image: none;
+        background-color: white; 
+        color: #000000; 
+        border: 2px solid black; 
+        font-size: 15px;`);
+    }
+    else  {
+        darkenBtn.removeAttribute('style');
+        darkenBtn.textContent = 'Darken';
+    }
+})
+
+lightenBtn.addEventListener('click', () => {
+    lightenFlag = !lightenFlag;
+    if (lightenFlag){
+        flag2 = true;
+        flag = true;
+        rgb.removeAttribute('style');
+        rgb.textContent = 'RGB';
+        eraser.textContent = 'Erase';
+        eraser.removeAttribute('style');
+        grabberFlag = false;
+        grabberBtn.textContent = 'Grabber';
+        grabberBtn.removeAttribute('style');
+        darkenFlag = false;
+        darkenBtn.removeAttribute('style');
+        darkenBtn.textContent = 'Darken';
+        lightenBtn.textContent = 'Lighten is ON';
+        lightenBtn.setAttribute('style', `
+        background-image: none;
+        background-color: white; 
+        color: #000000; 
+        border: 2px solid black; 
+        font-size: 15px;`);
+    }
+    else  {
+        lightenBtn.removeAttribute('style');
+        lightenBtn.textContent = 'Lighten';
+    }
+})
+
 rgb.addEventListener('mousedown', () => {
     rgb.setAttribute('style', "transform: scale(0.98); background-color: white; color: #000000; border: 2px solid black;  font-size: 15px")
 })
@@ -265,4 +483,28 @@ gridLines.addEventListener('mousedown', () => {
 
 gridLines.addEventListener('mouseup', () => {
     gridLines.removeAttribute('style');
+})
+
+grabberBtn.addEventListener('mousedown', () => {
+    grabberBtn.setAttribute('style', "transform: scale(0.999); background-color: white; color: #000000; border: 2px solid black;  font-size: 15px")
+})
+
+grabberBtn.addEventListener('mouseup', () => {
+    grabberBtn.removeAttribute('style');
+})
+
+darkenBtn.addEventListener('mousedown', () => {
+    darkenBtn.setAttribute('style', "transform: scale(0.999); background-color: white; color: #000000; border: 2px solid black;  font-size: 15px")
+})
+
+darkenBtn.addEventListener('mouseup', () => {
+    darkenBtn.removeAttribute('style');
+})
+
+lightenBtn.addEventListener('mousedown', () => {
+    lightenBtn.setAttribute('style', "transform: scale(0.999); background-color: white; color: #000000; border: 2px solid black;  font-size: 15px")
+})
+
+lightenBtn.addEventListener('mouseup', () => {
+    lightenBtn.removeAttribute('style');
 })
